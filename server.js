093e8e13aff0dbf65358fe307da92b1edf11d32a
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-12-04 08:04:40 -0800

    ok

--- server.js
+++ server.js
@@ -1,58 +1,107 @@
-// Requiring necessary npm packages
+// // Requiring necessary npm packages
+// var express = require("express");
+// var session = require("express-session");
+// // Requiring passport as we've configured it
+// var passport = require("./config/passport");
+
+
+// // Setting up port and requiring models for syncing
+
+// var PORT = process.env.PORT || 8080;
+// var db = require("./models"); 
+
+
+// var cors = require('cors');
+
+// // Creating express app and configuring middleware needed for authentication
+// var app = express();
+
+// app.use(cors());
+// var corsOptions = {
+//     origin: "http://localhost:8080"
+//   };
+  
+  
+//   // parse requests of content-type - application/json
+//   app.use(bodyParser.json());
+  
+//   // parse requests of content-type - application/x-www-form-urlencoded
+//   app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// } else {
+// app.use(express.static("client/public")); }
+// // We need to use sessions to keep track of our user's login status
+
+// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// // Requiring our routes
+
+
+// require("./routes/api-routes.js")(app);
+// require("./routes/profile-api-routes.js")(app);
+// require("./routes/character-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+
+
+// // Syncing our database and logging a message to the user upon success
+// // listens to the PORT
+// db.sequelize.sync({force:false}).then(function() {
+//     app.listen(PORT, function() {
+//         console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+//     });
+// });
+
+
+
+
+
+// Sets up the Express App
+// =============================================================
 var express = require("express");
 var session = require("express-session");
-// Requiring passport as we've configured it
 var passport = require("./config/passport");
 
-
-// Setting up port and requiring models for syncing
-
-var PORT = process.env.PORT || 8080;
-var db = require("./models"); 
-
-
-var cors = require('cors');
-
-// Creating express app and configuring middleware needed for authentication
 var app = express();
+var PORT = process.env.PORT || 8080;
 
-app.use(cors());
-var corsOptions = {
-    origin: "http://localhost:8080"
-  };
-  
-  
-  // parse requests of content-type - application/json
-  app.use(bodyParser.json());
-  
-  // parse requests of content-type - application/x-www-form-urlencoded
-  app.use(bodyParser.urlencoded({ extended: true }));
+// Requiring our models for syncing
+var db = require("./models");
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-} else {
-app.use(express.static("client/public")); }
-// We need to use sessions to keep track of our user's login status
 
+// Static directory
+//has to find index.html becasue its in a file
+app.use(express.static(__dirname + '/client/build'));
+//everything a static folder could be better but works
+app.use(express.static(__dirname + '/'));
+// =============================================================
+//Passport
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// initializes passport 
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Requiring our routes
-
-
+// =============================================================
+// Routes
+// =============================================================
 require("./routes/api-routes.js")(app);
 require("./routes/profile-api-routes.js")(app);
 require("./routes/character-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 
-// Syncing our database and logging a message to the user upon success
-// listens to the PORT
-db.sequelize.sync({force:false}).then(function() {
-    app.listen(PORT, function() {
-        console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-    });
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+//keeps on deleting database
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
